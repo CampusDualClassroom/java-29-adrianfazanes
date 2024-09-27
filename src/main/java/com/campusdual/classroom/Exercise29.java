153e8e8a9b99b284d6ba4db5158097906a6ad82d
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-27 10:36:21 +0200

    Ejercicio 29 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -3,5 +3,17 @@ package com.campusdual.classroom;
 public class Exercise29 {
     public static void main(String[] args) {
 
+
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+
     }
 }
